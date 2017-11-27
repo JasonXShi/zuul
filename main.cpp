dbeeb0b5fff769a303a7ba0eaed6872e31bea461
jasonxshi <322023@SHS-10DCE1254.beaverton.k12.or.us>
2017-11-27 12:24:28 -0800

    zuul

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <cstring>
+#include <iterator>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int main(){
+	vector<room*> rooms;
+	
+}
---
+++ room.cpp
@@ -0,0 +1,16 @@
+#include "digitalMedia.h"
+#include <cstring>
+#include <iostream>
+using namespace std;
+
+room::room(char* newDescription) {
+	description = newDescription;
+}
+
+void setExit(){
+}
+
+char* getDescription(){
+}
+
+
