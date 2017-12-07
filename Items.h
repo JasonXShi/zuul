5530631666a54fe37a5677169581906dd25d301d
Jason Shi <jasonxshi2002@gmail.com>
2017-12-07 12:19:46 -0800

    Update Items.h

--- Items.h
+++ Items.h
@@ -1,15 +1,17 @@
+//includes stuff and header guards
 #ifndef Items_H
 #define Items_H
-
 #include <iostream>
 using namespace std;
 
 class Items {
  public:
-  Items(char* newDescription);
+//defines constructer, destructer, and method of getting the description
+ Items(char* newDescription);
   char* getDescription();
   ~Items();
  private:
+ //initializes the description (char*)
   char* description;
 };
 
