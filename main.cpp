356fbf67c9b94228cfee2479afc2fd426effa0f9
Jason Shi <jasonxshi2002@gmail.com>
2017-12-07 12:18:04 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,9 @@
+/*
+Zuul - a text based adventure game where you can go around the map "dust 2" in CSGO
+Written By Jason Shi
+12/7/2017
+*/
+//import stuff
 #include <iostream>
 #include <vector>
 #include <map>
@@ -42,12 +48,13 @@ int main() {
   Room* pit = new Room("pit", "at pit, the pit at the end of long");
   pit->addItem(FLASHBANG);
 
-  //add exits
-  bSite->setExit("SOUTH", upperTuns);
+  //add exits to each room
+        bSite->setExit("SOUTH", upperTuns);
 	bSite->setExit("EAST", ctSpawn);
+	
 	upperTuns->setExit("NORTH", bSite);
 	upperTuns->setExit("EAST", lowerTuns);
-  upperTuns->setExit("SOUTH", outsideTuns);
+        upperTuns->setExit("SOUTH", outsideTuns);
 
 	outsideTuns->setExit("NORTH", upperTuns);
 	outsideTuns->setExit("SOUTH", tRamp);
@@ -119,7 +126,7 @@ int main() {
   Rooms.push_back(cross);
   Rooms.push_back(Long);
   Rooms.push_back(pit);
-
+//tell instructions
   cout << "Welcome to CSGO!" << endl;
   cout << "You are a Counterterrorist- " << endl;
   cout << "In order to win, you need to travel to CT Spawn and pick up a defuse kit." << endl;
@@ -130,11 +137,11 @@ int main() {
   cout << "Type DROP to drop an item" << endl;
   cout << "Type QUIT to leave the game" << endl;
   cout << "Type HELP to view items and exits" << endl;
-
   cout << endl;
+	
   while(true){
-    //check win condition
-    //iterate through the inventory and check if there is an item with description of defusekit
+    //check win and lose condition
+    //iterate through the inventory and check if there is an item with description of defusekit/bomb
     vector<Items*>::iterator it;
     for(it = inventory.begin(); it != inventory.end(); it++) {
       if (strcmp((*it)->getDescription(), DEFUSEKIT->getDescription())==0) {
@@ -147,14 +154,16 @@ int main() {
       return(0);
       }
     }
-
+//displays current room and the exits and items
     cout<< "You are " << current->getDescription() <<"." << endl;
     current->printInfo();
     cout << endl;
+     //displays inventory
     cout << "You have these items: " << endl;
     for(it = inventory.begin(); it != inventory.end(); it++) {
       cout<< (*it)->getDescription() << endl;
     }
+	  //gets user input
     char input[40];
     cout << endl;
     cout << "Please Enter a command: "<<endl;
@@ -162,10 +171,9 @@ int main() {
       if (strcmp(input, "GO") == 0){
         cout<< "Input the direction where you want to go: (NORTH, SOUTH, EAST, WEST)" << endl;
         cin.getline(input, 40);
-        //current room name
-        //iterate through until the right name is matched
-        //check if the direction inputted has a value
-        // if it does, set the current to the
+        //iterate through until the right room is found
+        //check if the direction inputted has a exit
+        // if it does, set the current to that room
         vector<Room*>::iterator iter;
         for(iter = Rooms.begin(); iter != Rooms.end(); iter++) {
           if (strcmp((*iter)->getName(), current->getName())==0) {
@@ -177,32 +185,31 @@ int main() {
       }else if(strcmp(input, "GET") == 0){
         cout << "Enter the name of the item you want to pick up:";
         cin.getline(input, 40);
-        //delete Item(input, inventory);
+	 //iterate through all items and if its the input, remove it from the room and add it to the inventory
         vector<Items*>::iterator ite;
-        for(ite = current->getItems().begin(); ite != current->getItems().end(); ++ite) {
-                               
+        for(ite = current->getItems().begin(); ite != current->getItems().end(); ++ite) {   
             if (strcmp((*ite)->getDescription(), input)==0) {
             inventory.push_back(*ite);
             current->removeItem(*ite);
             break;
-            
           }
         }
       } else if(strcmp(input, "DROP") == 0){
+	//iterate through the inventory and if the input is found, remove the item from the inventory and add it to the room
         cout<< "Enter the name of the item you want to drop:" <<endl;
         cin.getline(input, 40);
         for(it = inventory.begin(); it != inventory.end(); it++) {
           if (strcmp((*it)->getDescription(), input)==0) {
-            //add to room (current and actual)
             current->addItem(*it); 
             it = inventory.erase(it);
             break;
           }
         }
       }else if(strcmp(input, "HELP") == 0){
-        //just loops back through, because I tell all the items and exits at the beginning of the while loop
+        //just loops back through, because it displays all the items and exits at the beginning of the while loop
       }
       else if(strcmp(input, "QUIT") == 0){
+	//exits
         cout << "Exiting..."<< endl;
         return(0);
       }else{
