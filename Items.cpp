b35aed10d5dc3d81af474d66d806daebc63467a7
Jason Shi <jasonxshi2002@gmail.com>
2017-12-07 12:12:32 -0800

    Update Items.cpp

--- Items.cpp
+++ Items.cpp
@@ -2,13 +2,15 @@
 #include "Items.h"
 
 using namespace std;
-
+//constructor updates the description of the item
 Items::Items(char* newDescription) {
   description = newDescription;
 }
+//returns description
 char* Items::getDescription() {
   return description;
 }
+//deconstructor for items (deletes the description)
 Items::~Items(){
   delete[] description;
 }
